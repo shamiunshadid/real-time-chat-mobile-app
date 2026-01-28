d4fe656576a4065bd68cd36d35466fbe7547ffda
Shamiun Shadid <shamiunshadid@gmail.com>
2026-01-28 19:03:19 +0600

    Add initial backend setup with MongoDB connection, User model, and server entry point.

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI!)
-        console.log("MongoDB connected successfully");
-        
-    } catch (error) {
-        console.error("MongoDB connection failed", error)
-        process.exit(1)
+export const connectDB = async () => {
+  try {
+    const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+      throw new Error("MONGODB URI is not defined.");
     }
-}
+
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed", error);
+    process.exit(1);
+  }
+};
