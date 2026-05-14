bb1b7465a968aa34eca267e6ecf4e36df9e12099
Shamiun Shadid <shamiunshadid@gmail.com>
2026-05-14 11:11:23 +0600

    Refactor useApi to implement apiWithAuth for authenticated requests

--- frontend/hooks/useAuth.ts
+++ frontend/hooks/useAuth.ts
@@ -1,12 +1,13 @@
 import { useApi } from "@/lib/axios";
+import { User } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
 export const useAuthCallback = () => {
-  const api = useApi();
+  const {apiWithAuth} = useApi();
 
   const result = useMutation({
     mutationFn: async () => {
-      const { data } = await api.post("/auth/callback");
+      const { data } = await apiWithAuth<User>({method: "POST", url: "/auth/callback"})
       return data;
     },
   });
